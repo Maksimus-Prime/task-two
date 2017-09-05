02739e1c5615646112024f6c24483c0ff9dd1b7d
Maksimus-Prime <feedback.maksimus@gmail.com>
2017-09-05 21:42:14 +0700

    add resolve in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,44 +5,49 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: {
-  	main: "./frontend/main"
-  },
-  output: {
-  	path: __dirname + '/public',
-    filename: "[name].js"
-  },
+    entry: {
+        main: "./frontend/main"
+    },
+    output: {
+        path: __dirname + '/public',
+        filename: "[name].js"
+    },
 
-  watch: true,
+    watch: true,
 
-  module: {
+    resolve: {
+        extensions: ['', '.json', '.js', '.jsx', '.ts', '.tsx']
+    },
+
+    module: {
         loaders: [{
-        	test: /\.js$/,
-        	loader: 'babel-loader',
-        	query: {
-        		presets: ['es2015'],
-        		plugins: ['transform-runtime']
-          }
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015'],
+                plugins: ['transform-runtime']
+            }
+        }, 
+            { test: /\.json$/, loader: "json-loader" }
+        , {
+            test: /\.pug$/,
+            loader: "pug-loader",
+            query: {
+                pretty: true
+            }
         }, {
-        	test: /\.pug$/,
-        	loader: "pug-loader",
-          query: {
-            pretty: true
-          }
+            test: /\.(css|less)$/,
+            loader: ExtractTextPlugin.extract("style-loader","css-loader!autoprefixer-loader!less-loader")
         }, {
-          test: /\.(css|less)$/,
-          loader: ExtractTextPlugin.extract("style-loader","css-loader!autoprefixer-loader!less-loader")
-        }, {
-          test: /\.(jpe?g|png|svg|gif)$/,
-          loader: "file-loader?name=img/[name].[ext]"
+            test: /\.(jpe?g|png|svg|gif)$/,
+            loader: "file-loader?name=img/[name].[ext]"
         }]
-  },
-  plugins: [
+    },
+    plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './frontend/index.pug'
+        filename: 'index.html',
+        template: './frontend/index.pug'
     }),
-    new ExtractTextPlugin("main.css", {allChunks: true})
-  ]
-
+        new ExtractTextPlugin("main.css", {allChunks: true})
+    ]
 };
