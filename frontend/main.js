c86dc6ee6b621a9cb5f5db64efc84fc7631277fa
Maksimus-Prime <feedback.maksimus@gmail.com>
2017-10-20 10:20:30 +0700

    added require.context in main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1,24 +1,5 @@
 require("./style.less");
 require.context("./../blocks",true,/\.js$/);
 import './favicons/favicons';
-
-function grabAllFiles(startPath,filter){
-    if (!fs.existsSync(startPath)){
-        console.log("no dir ",startPath);
-        return;
-    }
-    var files=fs.readdirSync(startPath);
-    for(var i=0;i<files.length;i++){
-        var filename=path.join(startPath,files[i]);
-        var stat = fs.lstatSync(filename);
-        if (stat.isDirectory()){
-            grabAllFiles(filename,filter);
-        }
-        else if (filename.indexOf(filter)>=0) {
-            console.log(filename);
-            require("./../blocks/" + filename);
-        };
-    };
-};
-
-grabAllFiles("./../blocks", '.less');
+require.context("./../blocks",true,/\.less$/);
+require.context("./../blocks",true,/\.(jpe?g|png|svg|gif)$/);
